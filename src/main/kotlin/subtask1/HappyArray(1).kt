a87b11a3f137868ee3aaac42e02ebdde86375d85
DarVy21 <51174691+DarVy21@users.noreply.github.com>
2020-03-23 21:53:30 +0300

    Delete HappyArray(1).kt

--- src/main/kotlin/subtask1/HappyArray(1).kt
+++
@@ -1,9 +0,0 @@
-package subtask1
-
-class HappyArray {
-
-    // TODO: Complete the following function
-    fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
-    }
-}
